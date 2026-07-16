8d4ab61c6a9c8348e0ea942345c84be3497f0eba
daniel-d7a <eyad.taym.saba@gmail.com>
2026-07-16 13:50:45 +0300

    added image fallback to course video

--- src/components/courceInfo/CourceInfo.jsx
+++ src/components/courceInfo/CourceInfo.jsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import CourseVideo from "./CourseVideo";
-import { Check } from "lucide-react";
 import CourceFeatures from "./CourceFeatures";
 
 export default function CourceInfo({ course }) {
   return (
     <div className="grid grid-cols-12 gap-4">
       <div className="col-span-12 md:col-span-6 lg:col-span-7 ">
-        <CourseVideo videoUrl={course?.video_preview_link} imageUrl={course?.image} />
+        <CourseVideo videoUrl={course?.video_preview_link} imageUrl={course?.image} heightClass="aspect-video" />
       </div>
       <div className="col-span-12 md:col-span-6 lg:col-span-5 ">
         <CourceFeatures course={course} />
